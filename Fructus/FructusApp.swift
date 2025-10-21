d69c5b9fec362accf6ec84d6e78c3478203c0ff7
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-21 18:00:03 +0200

    Project Setup

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ghenadie Isacenco on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
